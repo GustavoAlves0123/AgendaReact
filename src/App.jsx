4d7a28aebed374d5b7c75fbbb868adc8a973d8bb
Gustavo Alves <99749706+GustavoAlves0123@users.noreply.github.com>
2023-11-28 20:08:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import ReactAgenda from './reactAgenda.js';
+import ReactAgendaCtrl from './reactAgendaCtrl.js';
+import Modal from './modal/Modal.jsx';
+import {guid , getUnique , getLast , getFirst } from './pages/helpers.jsx';
+import './style.css';
+
+module.exports = { ReactAgenda , ReactAgendaCtrl , guid , getUnique , getLast , getFirst , Modal } ;
